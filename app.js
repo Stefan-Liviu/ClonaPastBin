5f9df05e331af309dd751f940ad2509e7699a2e3
Stefan-Liviu <105070561+Stefan-Liviu@users.noreply.github.com>
2022-09-22 19:58:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyparser = require('body-parser');
-const flash = require('connect-flash');
 const indexRouter = require('./routes/index');
+const userRouter = require('./routes/users');
 
 const app = express();
 const port = 3000;
@@ -11,13 +11,13 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.use(flash());
 
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
 
 
 app.use('/', indexRouter);
+app.use('/', userRouter);
 
 app.listen(port, () => {
     console.log('App listening on port', port);
